70a57166fe83de296bf6a16ddfe7944c720720aa
MrRubel <rubsrub@mail.ru>
2020-03-04 20:54:55 +0300

    Uploaded project

---
+++ Assets/Colorizer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Colorizer: MonoBehaviour {
+    public ParticleSystem tunnelWall;
+    public ParticleSystem cellBig;
+    public ParticleSystem cellSmall;
+    public ParticleSystem cellVessel;
+
+    void Update() {
+        Color color = new Color(
+            Mathf.SmoothStep(1, 0.5f, Mathf.PingPong(Time.time / 10f, 1)),
+            Mathf.SmoothStep(0, 1, Mathf.PingPong(Time.time / 15f, 1)),
+            Mathf.SmoothStep(0, 1, Mathf.PingPong(Time.time / 5f, 1))
+        );
+
+        var tunnelWallMainSettings = tunnelWall.main;
+        tunnelWallMainSettings.startColor = color;
+
+        var cellBigMainSettings = cellBig.main;
+        cellBigMainSettings.startColor = color;
+
+        var cellSmallMainSettings = cellSmall.main;
+        cellSmallMainSettings.startColor = color;
+
+        var cellVesselMainSettings = cellVessel.main;
+        cellVesselMainSettings.startColor = color;
+    }
+}
---
+++ Assets/Mover.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Mover: MonoBehaviour {
+    public float rangeX = 2;
+    public float rangeY = 1.5f;
+    public float timeDelimiterX = 4f;
+    public float timeDelimiterY = 3f;
+
+    void Update() {
+        transform.position = new Vector3(
+            Mathf.SmoothStep(-rangeX, rangeX, Mathf.PingPong(Time.time / timeDelimiterX, 1)),
+            Mathf.SmoothStep(-rangeY, rangeY, Mathf.PingPong(Time.time / timeDelimiterY, 1)),
+            0
+        );
+    }
+}
